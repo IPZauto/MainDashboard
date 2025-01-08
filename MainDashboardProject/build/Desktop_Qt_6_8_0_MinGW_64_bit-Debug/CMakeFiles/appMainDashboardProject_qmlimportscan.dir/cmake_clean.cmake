6eca2163d1d9118b4f983588f00a7d0bd29658c6
Łukasz <162158296+LukaszStrzelczk@users.noreply.github.com>
2025-01-08 09:46:55 +0100

    Delete MainDashboardProject directory

--- MainDashboardProject/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/CMakeFiles/appMainDashboardProject_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/qml_imports/appMainDashboardProject_build.cmake"
-  "CMakeFiles/appMainDashboardProject_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appMainDashboardProject_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- MainDashboardProject/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/CMakeFiles/appMainDashboardProject_tooling.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "MainDashboardProject/Main.qml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appMainDashboardProject_tooling.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
